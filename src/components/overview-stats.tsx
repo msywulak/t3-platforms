b782be844b12f6cbb51c3f50b254e1e5aebefa83
Matt <51167140+msywulak@users.noreply.github.com>
2023-11-01 13:50:51 -0400

    Overview stats to shadcn and recharts

--- src/components/overview-stats.tsx
+++ src/components/overview-stats.tsx
@@ -1,57 +1,97 @@
 "use client";
 
 import { random } from "@/lib/utils";
-import { Card, Metric, Text, AreaChart, BadgeDelta, Flex } from "@tremor/react";
 import { useMemo } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis } from "recharts";
+
+const numberFormatter = (value: number) =>
+  Intl.NumberFormat("us").format(value).toString();
 
 export default function OverviewStats() {
-  const data = useMemo(() => {
+  const dataRand = useMemo(() => {
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
     return [
       ...months.map((month) => ({
         Month: `${month} 23`,
-        "Total Visitors": random(20000, 170418),
+        TotalVisitors: random(20000, 170418),
       })),
       {
         Month: "Jul 23",
-        "Total Visitors": 170418,
+        TotalVisitors: 170418,
       },
     ];
   }, []);
 
   return (
-    <div className="grid gap-6 sm:grid-cols-2">
-      <Card className="dark:!bg-stone-900">
-        <Text>Total Visitors</Text>
-        <Flex
-          className="space-x-3 truncate"
-          justifyContent="start"
-          alignItems="baseline"
-        >
-          <Metric className="font-cal">170,418</Metric>
-          <BadgeDelta
-            deltaType="moderateIncrease"
-            className="dark:bg-green-900 dark:bg-opacity-50 dark:text-green-400"
-          >
-            34.3%
-          </BadgeDelta>
-        </Flex>
-        <AreaChart
-          className="mt-6 h-28"
-          data={data}
-          index="Month"
-          valueFormatter={(number: number) =>
-            `${Intl.NumberFormat("us").format(number).toString()}`
-          }
-          categories={["Total Visitors"]}
-          colors={["blue"]}
-          showXAxis={true}
-          showGridLines={false}
-          startEndOnly={true}
-          showYAxis={false}
-          showLegend={false}
-        />
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Total Visitors</CardTitle>
+        <CardDescription>
+          Your excercise minutes are ahead of where you normally are.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pb-4">
+        <div className="h-[200px]">
+          <ResponsiveContainer width="100%" height="100%">
+            <AreaChart
+              data={dataRand}
+              margin={{
+                top: 5,
+                right: 10,
+                left: 10,
+                bottom: 0,
+              }}
+            >
+              <XAxis dataKey="Month" />
+              <Tooltip
+                content={({ active, payload }) => {
+                  if (active && payload?.length) {
+                    const totalVisitors = payload[0]?.value ?? 0; // Use 0 as a fallback value
+                    return (
+                      <div className="rounded-lg border bg-background p-2 shadow-sm">
+                        <div className="grid gap-2">
+                          <span className="text-[0.70rem] uppercase text-muted-foreground">
+                            Total Visitors
+                          </span>
+                          <span className="font-bold text-muted-foreground">
+                            {numberFormatter(totalVisitors as number)}
+                          </span>
+                        </div>
+                      </div>
+                    );
+                  }
+
+                  return null;
+                }}
+              />
+              <Area
+                type="monotone"
+                dataKey="TotalVisitors"
+                label="Total Visitors"
+                strokeWidth={2}
+                activeDot={{
+                  r: 8,
+                  style: { fill: "hsl(var(--primary))" },
+                }}
+                style={
+                  {
+                    stroke: "hsl(var(--primary))",
+                  } as React.CSSProperties
+                }
+                fill="hsl(var(--primary))"
+                opacity={0.5}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+      </CardContent>
+    </Card>
   );
 }
