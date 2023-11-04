99d476191c1e0d0461bc3bcd11c2a381010316c5
Matt <51167140+msywulak@users.noreply.github.com>
2023-11-04 11:29:18 -0400

    clean up profile

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,8 +1,6 @@
 import { redirect } from "next/navigation";
 import Link from "next/link";
-import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
-import { LogOutButtons } from "@/components/auth/logout-buttons";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,7 +8,6 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
@@ -84,28 +81,4 @@ export default async function Profile() {
       </DropdownMenuContent>
     </DropdownMenu>
   );
-
-  // return (
-  //   <div className="flex w-full items-center justify-between">
-  //     <Link
-  //       href="/settings"
-  //       className="flex w-full flex-1 items-center space-x-3 rounded-lg px-2 py-1.5 transition-all duration-150 ease-in-out hover:bg-stone-200 active:bg-stone-300 dark:text-white dark:hover:bg-stone-700 dark:active:bg-stone-800"
-  //     >
-  //       <Image
-  //         src={
-  //           user.imageUrl ??
-  //           `https://avatar.vercel.sh/${user.primaryEmailAddressId}`
-  //         }
-  //         width={40}
-  //         height={40}
-  //         alt={`${user.firstName} ${user.lastName}` ?? "User avatar"}
-  //         className="h-6 w-6 rounded-full"
-  //       />
-  // <span className="truncate text-sm font-medium">
-  //   {user.firstName} {user.lastName}
-  // </span>
-  //     </Link>
-  //     <LogOutButtons />
-  //   </div>
-  // );
 }
