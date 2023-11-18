c1c32afaf8040d0e84709e1388e74ce6b792c453
Matt <51167140+msywulak@users.noreply.github.com>
2023-11-18 14:30:17 -0500

    fix type issue with postId

--- src/components/forms/post/update-post-slug-form.tsx
+++ src/components/forms/post/update-post-slug-form.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { type Post } from "@/db/schema";
-import { z } from "zod";
+import { type z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
@@ -10,15 +10,12 @@ import { toast } from "sonner";
 import {
   Form,
   FormControl,
-  FormDescription,
-  FormField,
   FormItem,
   FormLabel,
-  FormMessage,
   UncontrolledFormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { postSchema, updatePostSchema } from "@/lib/validations/post";
+import { updatePostSchema } from "@/lib/validations/post";
 import { updatePost } from "@/lib/actions";
 import { catchClerkError } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -29,79 +26,6 @@ interface UpdatePostSlugFormProps {
 }
 
 type Inputs = z.infer<typeof updatePostSchema>;
-
-// export function UpdatePostSlugForm({ post }: UpdatePostSlugFormProps) {
-//   const [isPending, startTransition] = React.useTransition();
-
-//   // react-hook-form
-//   const form = useForm<Inputs>({
-//     resolver: zodResolver(postSchema),
-//     defaultValues: {
-//       id: post.id,
-//       slug: post.slug,
-//     },
-//   });
-
-// function onSubmit(data: Inputs) {
-//   console.log(data);
-//   startTransition(async () => {
-//     try {
-//       data.slug =
-//         data.slug
-//           ?.toLowerCase()
-//           .trim()
-//           .replace(/[\W_]+/g, "-") ?? "";
-//       await updatePost({ post: { ...data, site: null } });
-//       toast.success("Site Updated Successfully");
-//     } catch (err) {
-//       catchClerkError(err);
-//     }
-//   });
-// }
-
-//   return (
-//     <Form {...form}>
-//       <form
-//         className="flex w-full flex-col"
-//         onSubmit={(...args) => void form.handleSubmit(onSubmit)(...args)}
-//       >
-//         <div className="flex w-full items-end">
-//           <FormItem className="mr-4 flex-grow">
-//             <FormLabel>Post Slug</FormLabel>
-//             <FormControl>
-//               <Input
-//                 id="update-post-slug"
-//                 aria-invalid={!!form.formState.errors.slug}
-//                 aria-describedby="update-post-slug-text"
-//                 {...form.register("slug")}
-//                 name="slug"
-//                 type="text"
-//                 defaultValue={post.slug ?? ""}
-//                 minLength={3}
-//                 maxLength={32}
-//                 required={true}
-//                 aria-autocomplete="none"
-//               />
-//             </FormControl>
-//             <UncontrolledFormMessage
-//               message={form.formState.errors.slug?.message}
-//             />
-//           </FormItem>
-// <Button disabled={isPending} className="ml-auto">
-//   {isPending && (
-//     <Icons.spinner
-//       className="mr-2 h-4 w-4 animate-spin"
-//       aria-hidden="true"
-//     />
-//   )}
-//   Save Changes
-//   <span className="sr-only">Save Changes</span>
-// </Button>
-//         </div>
-//       </form>
-//     </Form>
-//   );
-// }
 
 export function InputForm({ post }: UpdatePostSlugFormProps) {
   const [isPending, startTransition] = React.useTransition();
--- src/lib/validations/post.ts
+++ src/lib/validations/post.ts
@@ -18,25 +18,26 @@ export const postSchema = z.object({
 });
 
 export const updatePostSchema = z.object({
-  id: z.number().optional(),
-  title: z.string().optional(),
-  content: z.string().optional(),
-  description: z.string().optional(),
+  id: z.number(),
+  title: z.string().optional().nullable(),
+  content: z.string().optional().nullable(),
+  description: z.string().optional().nullable(),
   slug: z
     .string()
     .regex(
       /^[a-z0-9 ]+(?:-[a-z0-9 ]+)*$/,
       "Slug can only contain letters, numbers, hyphens, and spaces.",
     )
-    .optional(),
-  image: z.string().optional(),
-  imageBlurhash: z.string().optional(),
-  published: z.boolean().optional(),
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
-  userId: z.number().optional(),
-  siteId: z.number().optional(),
-  clerkId: z.string().optional(),
+    .optional()
+    .nullable(),
+  image: z.string().optional().nullable(),
+  imageBlurhash: z.string().optional().nullable(),
+  published: z.boolean().optional().nullable(),
+  createdAt: z.date().optional().nullable(),
+  updatedAt: z.date().optional().nullable(),
+  userId: z.number().optional().nullable(),
+  siteId: z.number().optional().nullable(),
+  clerkId: z.string().optional().nullable(),
 });
 
 const siteSchema = z.object({
